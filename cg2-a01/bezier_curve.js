24452d34c956668d69bcbd88b55a40a9abfe2b5f
HalaBasali <HalaBasali@gmail.com>
2013-04-25 14:39:17 +0200

    kleine Änderungen..

--- cg2-a01/bezier_curve.js
+++ cg2-a01/bezier_curve.js
@@ -23,9 +23,10 @@ define([ "util", "vec2", "scene", "straight_line" , "tickmarks" ,"parametric_cur
 	    this.p1 = point1 || [0,1];
 	    this.p2 = point2 || [0,-1];
         this.p3 = point3 || [1,0];
+        var t = minT;
 		
-		this.funX = Math.pow((1 - t), 3) * p0[0] + 3 * Math.pow(1 - t, 2) * t * p1[0] + 3 * (1 - t) * Math.pow(t, 2) * p2[0] + Math.pow(t, 3) * p3[0];
-		this.funY = Math.pow((1 - t), 3) * p0[1] + 3 * Math.pow(1 - t, 2) * t * p1[1] + 3 * (1 - t) * Math.pow(t, 2) * p2[1] + Math.pow(t, 3) * p3[1];
+		this.funX = Math.pow((1 - t), 3) * this.p0[0] + 3 * Math.pow(1 - t, 2) * t * this.p1[0] + 3 * (1 - t) * Math.pow(t, 2) * this.p2[0] + Math.pow(t, 3) * this.p3[0];
+		this.funY = Math.pow((1 - t), 3) * this.p0[1] + 3 * Math.pow(1 - t, 2) * t * this.p1[1] + 3 * (1 - t) * Math.pow(t, 2) * this.p2[1] + Math.pow(t, 3) * this.p3[1];
 		
 		this.segments = segments || 5;
 		this.tickmarks = tickmarks;
@@ -101,6 +102,52 @@ define([ "util", "vec2", "scene", "straight_line" , "tickmarks" ,"parametric_cur
 		return false;
 	}; 
 	
+	BezierCurve.prototype.createDraggers = function() {
+
+		var draggers = [];
+
+		// set tickmarks if its set and if we are not at the first or last point
+		if (this.tickmarks) {
+			for ( var j = 1; j < this.tArr.length; j++) {
+
+				// calculate current point
+				var t = this.tArr[j];
+				var x = eval(this.funX);
+				var y = eval(this.funY);
+
+				// calculate last point
+				t = this.tArr[j - 1];
+				var beforeX = eval(this.funX);
+				var beforeY = eval(this.funY);
+
+				// calculate next point
+				t = this.tArr[j + 1];
+				var afterX = eval(this.funX);
+				var afterY = eval(this.funY);
+
+				// tangente von x = PunktDanach - PunktDavor / n (zum Kürzen)
+				var tangenteX = (afterX - beforeX) / 8;
+				var tangenteY = (afterY - beforeY) / 8;
+
+				// normale von [x, y] = [-y, x]
+				var normaleX = -tangenteY;
+				var normaleY = tangenteX;
+
+				// connect two points for the normale: One over and one under the curve
+				var pointOverCurve = [ x + normaleX, y + normaleY ];
+				var pointUnderCurve = [ x - normaleX, y - normaleY ];
+
+				var mark = new StraightLine(pointOverCurve, pointUnderCurve, {
+					width : "1",
+					color : "#DF013A"
+				});
+
+				draggers.push(new Tickmarks(mark));
+			}
+		}
+
+		return draggers;
+	};
 	return BezierCurve;
 	
 })); // end define
