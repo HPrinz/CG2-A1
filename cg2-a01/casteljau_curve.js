7a8428251d2b61447d5fa987a5b0316373e3fb61
HalaBasali <HalaBasali@gmail.com>
2013-05-05 22:26:00 +0200

    casteljau work

--- cg2-a01/casteljau_curve.js
+++ cg2-a01/casteljau_curve.js
@@ -53,37 +53,44 @@ define([ "util", "scene" , "straight_line" , "control_polygon" ], (function(Util
 		
 		this.poly0.draw(context);
 		
-		var a0 = (1 - this.t) * this.p0 + this.t * this.p1;
-		var a1 = (1 - this.t) * this.p1 + this.t * this.p2;
-		var a2 = (1 - this.t) * this.p2 + this.t * this.p3;
+		var p0 = this.p0;
+		var p1 = this.p1;
+		var p2 = this.p2;
+		var p3 = this.p3;
+		var t  = this.t;
 		
-		var b0 = (1 - this.t) * a0 + this.t * a1;
-		var b1 = (1 - this.t) * a1 + this.t * a2;
-		
-		var c0 = (1 - this.t) * b0 + this.t * b1;
-		
-		var poly1 = new ControlPolygon(this.p0, a0, b0, c0, this.drawStyle);
-		var poly2 = new ControlPolygon(c0, b1, a2, this.p3, this.drawStyle);
-		poly1.draw(context);
-		poly2.draw(context);
-
+		// defines the depth of divisions of the control polygon.
+		var depth = 5;
 		var style = {
 				radius : 5,
 				width : 2,
 				color : "#0000ff",
 				fill : false
-			};
-		if(this.showControlPolygons) {
-			new StraightLine(a0, a1, style).draw(context);
-			new StraightLine(a1, a2, style).draw(context);
-			new StraightLine(b0, b1, style).draw(context);
-		}
+		};
 		
-		var depth = 5;
 		for(var i = 0; i < depth; i++) {
 			
+			var a0 = (1 - t) * p0 + t * p1;
+			var a1 = (1 - t) * p1 + t * p2;
+			var a2 = (1 - t) * p2 + t * p3;
+			
+			var b0 = (1 - t) * a0 + t * a1;
+			var b1 = (1 - t) * a1 + t * a2;
+			
+			var c0 = (1 - t) * b0 + t * b1;
+			
+			var poly1 = new ControlPolygon(p0, a0, b0, c0, style);
+			var poly2 = new ControlPolygon(c0, b1, a2, p3, style);
+//			poly1.draw(context);
+//			poly2.draw(context);
+			console.log("poly1: " + poly1 + ", poly2: " + poly2);
+			
+			if(this.showControlPolygons) {
+				new StraightLine(a0, a1, style).draw(context);
+				new StraightLine(a1, a2, style).draw(context);
+				new StraightLine(b0, b1, style).draw(context);
+			}
 		}
-
 	};
 	
 	/* 
@@ -93,7 +100,8 @@ define([ "util", "scene" , "straight_line" , "control_polygon" ], (function(Util
     };
     
     CasteljauCurve.prototype.createDraggers = function() {
-    	
+    	var draggers = [];
+    	return draggers;
     }
 
 	// this module exposes only the constructor for Dragger objects
